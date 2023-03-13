ea97eaa26578291b4e9f4f88079d5d61c8c2fb0c
Tatyana Chernyadieva <tany.taray@gmail.com>
2023-03-13 17:53:05 +0400

    Spring-security-app
    ветка not-provider: добавлен пример работы с security без провайдера

--- src/main/java/ru/home/chernyadieva/springsecuritytest/config/SecurityConfig.java
+++ src/main/java/ru/home/chernyadieva/springsecuritytest/config/SecurityConfig.java
@@ -1,28 +1,39 @@
 package ru.home.chernyadieva.springsecuritytest.config;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.home.chernyadieva.springsecuritytest.service.UsersDetailsService;
 
 /**
  * Класс для конфигурирования аутентификации и авторизации пользователя spring security
  */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration {
-    private final AuthProviderImpl authProvider;
+    private final UsersDetailsService usersDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(UsersDetailsService usersDetailsService) {
+        this.usersDetailsService = usersDetailsService;
     }
 
     /**
-     * Метод настраивает аутентификацию
+     * Метод настраивает аутентификацию напрямую через сервис, а не через провайдер
      *
      * @param auth
      */
+    @SneakyThrows
     protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+        auth.userDetailsService(usersDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
--- src/main/java/ru/home/chernyadieva/springsecuritytest/controller/MyController.java
+++ src/main/java/ru/home/chernyadieva/springsecuritytest/controller/MyController.java
@@ -1,7 +1,10 @@
 package ru.home.chernyadieva.springsecuritytest.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.home.chernyadieva.springsecuritytest.security.UsersDetails;
 
 /**
  * Класс-контроллер для связи с БД
@@ -11,6 +14,15 @@ public class MyController {
 
     @RequestMapping("/hello")
     public String helloPage() {
+        return "hello";
+    }
+
+    @RequestMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UsersDetails usersDetails = (UsersDetails) authentication.getPrincipal();
+        System.out.println(usersDetails.getPerson());
+
         return "hello";
     }
 }
